5e871f251750def1036a731af1ee802ff36b85f3
Andras Barthazi <andras@barthazi.hu>
2014-10-26 12:57:11 +0100

    run tests with parameterized

--- tests/test_escherauth_amazon.py
+++ tests/test_escherauth_amazon.py
@@ -2,11 +2,12 @@ import unittest
 import datetime
 
 from escherauth.escherauth import Escher
+from nose_parameterized import parameterized
 
 
 def read_request(suite, test, extension='req'):
     file = open('tests/' + suite + '_testsuite/' + test + '.' + extension, 'r')
-    lines = (file.read()+"\n").splitlines()
+    lines = (file.read() + "\n").splitlines()
     file.close()
 
     method, uri = lines[0].split(' ')[0:2]
@@ -36,10 +37,14 @@ class EscherAuthAmazonTest(unittest.TestCase):
             'current_time': datetime.datetime(2011, 9, 9, 23, 36)
         })
 
-
-    def test_signing(self):
-        request = read_request('aws4', 'get-vanilla-empty-query-key')
-        request_signed = read_request('aws4', 'get-vanilla-empty-query-key', 'sreq')
+    @parameterized.expand([
+        ('aws4', 'get-space'),
+        ('aws4', 'get-unreserved'),
+        ('aws4', 'get-utf8'),
+    ])
+    def test_signing(self, suite, testcase):
+        request = read_request(suite, testcase)
+        request_signed = read_request(suite, testcase, 'sreq')
         request = self.escher.sign(request, {
             'api_key': 'AKIDEXAMPLE',
             'api_secret': 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY'
@@ -49,4 +54,3 @@ class EscherAuthAmazonTest(unittest.TestCase):
         self.assertEqual(request.get('uri'), request_signed.get('uri'))
         self.assertListEqual(request.get('headers'), request_signed.get('headers'))
         self.assertEqual(request.get('body'), request_signed.get('body'))
-
