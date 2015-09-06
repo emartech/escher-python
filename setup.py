6683a02e95ae67d2c9edb756997aaa11890cbab4
Andras Barthazi <andras@barthazi.hu>
2015-09-06 22:29:52 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='escherauth',
     description='Python implementation of the AWS4 compatible Escher HTTP request signing protocol.',
-    version='0.2.4',
+    version='0.2.5',
     author='Andras Barthazi',
     author_email='andras@barthazi.hu',
     license='MIT',
